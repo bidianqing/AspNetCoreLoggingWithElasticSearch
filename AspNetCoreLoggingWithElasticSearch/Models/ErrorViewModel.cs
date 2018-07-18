63bef21284fdc6ab30a00172d31d9dd911b214f6
毕殿卿 <bdq123@outlook.com>
2018-07-18 21:20:06 +0800

    添加项目文件。

---
+++ AspNetCoreLoggingWithElasticSearch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreLoggingWithElasticSearch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AspNetCoreLoggingWithElasticSearch/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System;
+
+namespace AspNetCoreLoggingWithElasticSearch
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            Log.Logger = new LoggerConfiguration()
+               //.Enrich.FromLogContext()
+               //.Enrich.WithExceptionDetails()
+               //.Enrich.WithMachineName()
+               // Instead of configuring the sink directly in code, call ReadFrom.Configuration():
+               .ReadFrom.Configuration(Configuration)
+               //.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+               //{
+               //    AutoRegisterTemplate = true,
+               //})
+            .CreateLogger();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddSerilog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
